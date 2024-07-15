9656b4b722ce5f9ca597513698769b2d023e2ccb
AMISH-KHAN <khanamish8923@gmail.com>
2024-07-15 18:24:38 +0530

    increased timeout

--- server_AIcruiter/dbconnect.js
+++ server_AIcruiter/dbconnect.js
@@ -4,7 +4,12 @@ const mongoose = require("mongoose")
 async function getdatabase() {
     const url = process.env.MONGODB_URL
     try {
-        await mongoose.connect(url)
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 20000, // Increase the timeout to 20 seconds
+  socketTimeoutMS: 45000,
+        })
         console.log("database connected")
     }
     catch(error) {
